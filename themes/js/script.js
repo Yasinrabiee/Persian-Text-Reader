108f16b5aa60ddf2270bf150ad7d5d72dfe7d545
Yasin Rabiee <hlww6303@gmail.com>
2024-11-17 14:56:48 +0330

    Update script.js

--- themes/js/script.js
+++ themes/js/script.js
@@ -30,7 +30,6 @@ textarea.on('input', function() {
 	const text = $(this).val();
 	result.html(text);
 	$(`#dotless_container, #removeSpace_container`).show();
-	changeScroll();
 });
 
 textarea.on('paste', function() {
@@ -39,7 +38,6 @@ textarea.on('paste', function() {
 		$('#result').html(text);
 	},10);
 	$(`#dotless_container, #removeSpace_container`).show();
-	changeScroll();
 });
 
 fileInput.addEventListener('change', (e) =>{
